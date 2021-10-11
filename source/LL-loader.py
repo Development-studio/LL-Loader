76e470c48aabc35b5269e40d66b77510e3027395
mikhaillav <59438110+mikhaillav@users.noreply.github.com>
2021-10-11 23:11:23 +0300

    Add choice

--- source/LL-loader.py
+++ source/LL-loader.py
@@ -5,13 +5,16 @@ import subprocess
 
 pth=os.getcwd()
 
+print("write down which versions you want to use(use full name of version, example 1.2.4 and 1.17.34.02)")
+LL = input("what version of LL u need? ")
+BDS = input("what version of BDS u need? ")
+
 print('Beginning downloading files...') 
-zip = 'https://minecraft.azureedge.net/bin-win/bedrock-server-1.17.34.02.zip'
-zip1 = 'https://github.com/LiteLDev/LiteLoaderBDS/releases/download/1.2.4/LiteLoader.zip'
+zip = 'https://minecraft.azureedge.net/bin-win/bedrock-server-'+ BDS +'.zip'
+zip1 = 'https://github.com/LiteLDev/LiteLoaderBDS/releases/download/'+ LL +'/LiteLoader.zip'
 urllib.request.urlretrieve(zip, 'bedrock_server.zip') 
 urllib.request.urlretrieve(zip1, 'LiteLoader.zip')
 print("Success!")
-
 
 print('Starting extract files')
 fantasy_zip = zipfile.ZipFile(pth + '\\bedrock_server.zip')
@@ -23,15 +26,7 @@ fantasy_zip.close()
 print('All file extracted! Starting generate server')
 os.system(pth + '\\SymDB2.exe')
 
-
 for i in range( 0, 0 ):
     subprocess.call(('SymDB2.exe', str(i)))
 
 os.system(pth + '\\bedrock_server.exe')
-
-my_file = open('LL-loader.txt', 'w+')
-my_file.write('Hello!' + '\n')
-my_file.write('Thx for using LL-loader' + '\n')
-my_file.write('Github: https://github.com/Development-studio/LL-Loader')
-my_file.close()
-
